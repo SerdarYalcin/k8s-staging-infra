cae55a9216f8b8c9d6208dabc5c626f4c27ba6be
Serdar <serdar.yalcin@ngpems.com.mt>
2024-05-23 11:12:21 +0200

    updated3

--- main.tf
+++ main.tf
@@ -1,35 +1,62 @@
-resource "google_identity_platform_tenant" "flexer_stage" {
-  project      = var.project_id
-  display_name = "Flexer Stage"
-}
-
-resource "google_identity_platform_tenant_default_supported_idp_config" "email" {
-  project    = var.project_id
-  tenant     = google_identity_platform_tenant.flexer_stage.name
-  provider   = "password"
-  enabled    = true
+resource "google_project_service" "identitytoolkit" {
+  project = var.project_id
+  service = "identitytoolkit.googleapis.com"
 }
 
 resource "google_identity_platform_config" "default" {
   project = var.project_id
-
+  autodelete_anonymous_users = true
   sign_in {
-    allow_duplicate_emails = false
+    allow_duplicate_emails = true
 
-    email {
-      enabled = true
-      password {
-        enabled = true
-      }
+    anonymous {
+        enabled = false
     }
-  }
+    email {
+        enabled = true
+        password_required = true
+    }
+    # phone_number {
+    #     enabled = true
+    #     test_phone_numbers = {
+    #         "+11231231234" = "000000"
+    #     }
+    #}
+  # }
+  # sms_region_config {
+  #   allowlist_only {
+  #     allowed_regions = [
+  #       "US",
+  #       "CA",
+  #     ]
+  #   }
+  # }
+  # blocking_functions {
+  #   triggers {
+  #     event_type = "beforeSignIn"
+  #     function_uri = "https://us-east1-my-project.cloudfunctions.net/before-sign-in"
+  #   }
+  #   forward_inbound_credentials {
+  #     refresh_token = true
+  #     access_token = true
+  #     id_token = true
+  #   }
+  # }
+#   quota {
+#     sign_up_quota_config {
+#       quota = 1000
+#       start_time = ""
+#       quota_duration = "7200s"
+#     }
+#   }
+#   authorized_domains = [
+#     "localhost",
+#     "my-project.firebaseapp.com",
+#     "my-project.web.app",
+#   ]
+# }
 
-  user_management {
-    allow_user_delete = true
-    allow_user_sign_up = true
-  }
-
-  linking {
-    link_accounts_with_same_email = true
-  }
+resource "google_identity_platform_tenant" "tenant" {
+  display_name          = "flexer-stage"
+  allow_password_signup = true
 }
